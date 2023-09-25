14032da65a85db70aa7448be0e51516c976752c0
Daniel Hernández Fajardo <daniel.hernandez@codebay-innovation.com>
2023-09-25 17:23:19 +0200

    fixes for sonarcloud

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -16,8 +16,9 @@ public class FractionTest {
     @Test
     void testDefaultConstructor() {
         Fraction fraction = new Fraction();
-        assertEquals(1, fraction.getNumerator());
         assertEquals(1, fraction.getDenominator());
+        assertEquals(1, fraction.getNumerator());
+
     }
 
     @Test
@@ -35,15 +36,15 @@ public class FractionTest {
     @Test
     void testSetNumerator() {
         Fraction fraction = new Fraction(2, 3);
-        fraction.setNumerator(4);
-        assertEquals(4, fraction.getNumerator());
+        fraction.setNumerator(5);
+        assertEquals(5, fraction.getNumerator());
     }
 
     @Test
     void testSetDenominator() {
         Fraction fraction = new Fraction(2, 3);
-        fraction.setDenominator(4);
-        assertEquals(4, fraction.getDenominator());
+        fraction.setDenominator(5);
+        assertEquals(5, fraction.getDenominator());
     }
 
     @Test
@@ -78,49 +79,49 @@ public class FractionTest {
 
     @Test
     void testIsEquivalent() {
-        Fraction fraction1 = new Fraction(2, 3);
-        Fraction fraction2 = new Fraction(4, 6);
+        Fraction fraction1 = new Fraction(1, 2);
+        Fraction fraction2 = new Fraction(2, 4);
         assertTrue(fraction1.isEquivalent(fraction2));
 
-        Fraction fraction3 = new Fraction(1, 2);
-        Fraction fraction4 = new Fraction(3, 4);
+        Fraction fraction3 = new Fraction(2, 6);
+        Fraction fraction4 = new Fraction(2, 5);
         assertFalse(fraction3.isEquivalent(fraction4));
     }
 
     @Test
     void testAdd() {
-        Fraction fraction1 = new Fraction(1, 4);
-        Fraction fraction2 = new Fraction(3, 5);
+        Fraction fraction1 = new Fraction(5, 2);
+        Fraction fraction2 = new Fraction(1, 3);
         Fraction sum = fraction1.add(fraction2);
         assertEquals(17, sum.getNumerator());
-        assertEquals(20, sum.getDenominator());
+        assertEquals(6, sum.getDenominator());
     }
 
     @Test
     void testSubtract() {
-        Fraction fraction1 = new Fraction(2, 4);
-        Fraction fraction2 = new Fraction(1, 4);
+        Fraction fraction1 = new Fraction(5, 2);
+        Fraction fraction2 = new Fraction(1, 3);
         Fraction result = fraction1.subtract(fraction2);
-        assertEquals(4, result.getNumerator());
-        assertEquals(16, result.getDenominator());
+        assertEquals(13, result.getNumerator());
+        assertEquals(6, result.getDenominator());
     }
 
     @Test
     void testMultiply() {
-        Fraction fraction1 = new Fraction(1, 2);
-        Fraction fraction2 = new Fraction(3, 4);
+        Fraction fraction1 = new Fraction(3, 5);
+        Fraction fraction2 = new Fraction(7, 2);
         Fraction product = fraction1.multiply(fraction2);
-        assertEquals(3, product.getNumerator());
-        assertEquals(8, product.getDenominator());
+        assertEquals(21, product.getNumerator());
+        assertEquals(10, product.getDenominator());
     }
 
     @Test
     void testDivide() {
-        Fraction fraction1 = new Fraction(1, 2);
-        Fraction fraction2 = new Fraction(2, 3);
+        Fraction fraction1 = new Fraction(3, 5);
+        Fraction fraction2 = new Fraction(7, 2);
         Fraction result = fraction1.divide(fraction2);
-        assertEquals(3, result.getNumerator());
-        assertEquals(4, result.getDenominator());
+        assertEquals(6, result.getNumerator());
+        assertEquals(35, result.getDenominator());
     }
 
 }
